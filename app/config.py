5880c295a2daaee95a00d1d53eaca2d39daafdad
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2021-12-01 23:19:07 +0700

    environ

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
-DB_HOST = "iot.cpe.ku.ac.th"
-DB_USER = "b6210545432"
-DB_PASSWD = "jakkrathorn.s@ku.th"
-DB_NAME = "b6210545432"
-DB_TABLE = "AnilikeResponse"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWD = os.getenv("DB_PASSWD")
+DB_NAME = os.getenv("DB_NAME")
+DB_TABLE = os.getenv("DB_TABLE")
